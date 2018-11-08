43dc99f686115cb3c00b6b09bcdaaf483c4f5ed5
kooseolwon <seolwon6289@naver.com>
2018-11-08 23:15:55 +0900

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api/index');
+
+res.use('/api',apiRouter);
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
