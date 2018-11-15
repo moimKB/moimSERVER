6aab66f1255dd559d29b2d08379269760bb7a9db
kooseolwon <seolwon6289@naver.com>
2018-11-15 16:41:05 +0900

    change account schema

--- schema/account.js
+++ schema/account.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
+const moment = require('moment')
 const Schema = mongoose.Schema;
 
 let accountSchema = new Schema({
-    write_time : Date,
-    deposit : Number,//입금
-    withdraw : Number,//출금
-    account_content : String,
-    user_id :String
+    write_time : {type : Date, default : new Date(moment().format())},
+    price : {type : Number, default : null},//돈
+    bank : String,
+    account_number : String,
+    account_content : {type : String, default : null},
+    to_user_id :{type : String, default : null},
+    from_user_id : String
 },{
     versionKey:false
 });
