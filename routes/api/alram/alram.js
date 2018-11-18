cb6b4eb7cd6e540db3273806900ffc3b0b24db4e
kooseolwon <seolwon6289@naver.com>
2018-11-18 23:23:37 +0900

    create alram

--- routes/api/alram/alram.js
+++ routes/api/alram/alram.js
@@ -18,26 +18,47 @@ router.get('/',async(req,res)=>{
         })
     }
     let output = await notice.find({notice_category : 1});
+    if(!output){
+        res.status(500).send({
+            message:"Internal Server Error"
+        })
+    }
     for(let i = 0 ; i<output.length ; i++){
 
         let Id = output[i].club_id;
         peopleArr = output[i].notice_people;
+
         for(let a = 0 ; a < peopleArr.length ; a++){
-            
-            if(decoded.id === peopleArr)
-            let clubOutput = await club.find({_id : Id},{club_logo: true,club_name: true});
-            let temp ={
-                club_logo : clubOutput[0].club_logo,
-                club_name : clubOutput[0].club_name,
-                club_manager : output[i].club_manager,
-                write_time : output[i].write_time,
-                notice_title : output[i].notice_title,
-                notice_content : output[i].notice_content,
+            if(decoded.id === peopleArr[a].user_id){
+                console.log(decoded.id)
+                console.log(peopleArr[a].user_id)
+                console.log(typeof(decoded.id))
                 
+                let clubOutput = await club.find({_id : Id},{club_logo: true,club_name: true});
+                
+                if(!clubOutput){
+                    res.status(500).send({
+                        message: "Internal Server Error"
+                    })
+                }
+                let temp ={
+                    club_logo : clubOutput[0].club_logo,
+                    club_name : clubOutput[0].club_name,
+                    club_manager : output[i].club_manager,
+                    write_time : output[i].write_time,
+                    notice_title : output[i].notice_title,
+                    notice_content : output[i].notice_content,
+                    account_check : peopleArr[a].account_check
+                }
+                tempArr.push(temp);
             }
         }
-        
     }
+    res.status(200).send({
+        message: "success to show finance list",
+        data : tempArr
+    });
+
 
 
     
